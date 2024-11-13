40654e9e2b0d2365f1c3d8fee5d4b0de66f24a77
Eshan <2016mehrab@gmail.com>
2024-11-13 21:39:12 +0600

    feat: solve 'bulb switcher' and add diagrams.

---
+++ lc319.java
@@ -0,0 +1,16 @@
+class LC319 {
+
+    public int bulbSwitch(int n) {
+
+        return (int)Math.floor(Math.sqrt(n));
+
+    }
+
+    public static void main(String[] args) {
+        LC319 s = new LC319();
+        System.out.println(s.bulbSwitch(20));
+        ;
+
+    }
+
+}
