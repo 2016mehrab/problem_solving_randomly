f2e482140758b94b00277df7421be5cd8506cdda
Eshan <2016mehrab@gmail.com>
2024-12-09 14:22:25 +0600

    update: add binary search solution to 'Special Array II'

--- lc3152.java
+++ lc3152.java
@@ -16,7 +16,7 @@ class LC3152 {
         return validIndex;
     }
 
-    public boolean[] isArraySpecial(int[] nums, int[][] queries) {
+    public boolean[] isArraySpecialPrefixApproach(int[] nums, int[][] queries) {
         N = nums.length;
         int M = queries.length;
         int[] arr = statusPerIndex(nums);
@@ -29,6 +29,47 @@ class LC3152 {
 
         }
         Arrays.stream(arr).forEach(System.out::println);
+        return r;
+    }
+
+    private boolean hasViolatingIndex(int upperbound, int lowerbound, int[] violatingIndex, int size) {
+        int l = 0;
+        int r = size - 1;
+        int mid = -1;
+        while (l <= r) {
+            mid = (l + r) / 2;
+            if (violatingIndex[mid] != -1 && violatingIndex[mid] <= upperbound && violatingIndex[mid] >= lowerbound) {
+                return true;
+            } else if (violatingIndex[mid] > upperbound)
+                r = mid - 1;
+            else if (violatingIndex[mid] < lowerbound)
+                l = mid + 1;
+        }
+        return false;
+    }
+
+    public boolean[] isArraySpecial(int[] nums, int[][] queries) {
+        N = nums.length;
+        int M = queries.length;
+        int[] violatingIndex = new int[N];
+        int index = 0;
+        boolean[] r = new boolean[M];
+
+        Arrays.fill(violatingIndex, -1);
+        for (int i = 0; i < N; ++i) {
+            if (i < N - 1 && nums[i] % 2 == nums[i + 1] % 2)
+                violatingIndex[index++] = i + 1;
+        }
+
+        for (int i = 0; i < M; ++i) {
+            int start = queries[i][0];
+            int end = queries[i][1];
+            r[i] = !hasViolatingIndex(end, start + 1, violatingIndex, index);
+        }
+        for (boolean x : r)
+            System.out.println(x);
+
+        // Arrays.stream(violatingIndex).forEach(System.out::println);
         return r;
     }
 
