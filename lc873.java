a68c3ba77cf4b272fe980baf7f0d3aaa165926eb
Eshan <2016mehrab@gmail.com>
2025-02-28 14:25:32 +0600

    feat: solve 'Length of Longest Fibonacci Subsequence'
    DP

--- lc873.java
+++ lc873.java
@@ -6,43 +6,40 @@ import java.util.Set;
 
 class LC873 {
     public int lenLongestFibSubseq(int[] arr) {
-        int n = arr.length;
-        // Store array elements in set for O(1) lookup
-        Set<Integer> numSet = new HashSet<>();
-        for (int num : arr) {
-            numSet.add(num);
-        }
-
+        int N = arr.length;
+        // num, index
+        Map<Integer, Integer> map = new HashMap<>();
         int maxLen = 0;
-        // Try all possible first two numbers of sequence
-        for (int start = 0; start < n; ++start) {
-            for (int next = start + 1; next < n; ++next) {
-                // Start with first two numbers
-                int prev = arr[next];
-                int curr = arr[start] + arr[next];
+        for (int i = 0; i < N; ++i)
+            map.put(arr[i], i);
+        int[][] dp = new int[N][N];
+        for (int i = N - 1; i > 0; --i) {
+            for (int j = i - 1; j > -1; --j) {
+                int prev = arr[j];
+                int curr = arr[i];
+                int nxt = prev + curr;
                 int len = 2;
-
-                // Keep finding next Fibonacci number
-                while (numSet.contains(curr)) {
-                    // Update values for next iteration
-                    int temp = curr;
-                    curr += prev;
-                    prev = temp;
-                    maxLen = Math.max(maxLen, ++len);
+                if (map.containsKey(nxt) && map.get(nxt) > i) {
+                    len = 1 + dp[i][map.get(nxt)];
+                    maxLen = Math.max(len, maxLen);
                 }
+                dp[j][i] = len;
+
             }
+
         }
+        System.out.println(maxLen);
 
         return maxLen;
     }
 
     public static void main(String[] args) {
         LC873 solution = new LC873();
-        // solution.lenLongestFibSubseq(new int[] { 1, 2, 3, 4, 5, 6, 7, 8 });
-        // solution.lenLongestFibSubseq(new int[] { 1, 3, 7, 11, 12, 14, 18 });
-        // solution.lenLongestFibSubseq(new int[] { 2, 4, 7, 8, 9, 10, 14, 15, 18, 23,
-        // 32, 50 });
-        // solution.lenLongestFibSubseq(new int[] { 1, 3, 7, 8, 15 });
+        solution.lenLongestFibSubseq(new int[] { 1, 2, 3, 4, 5, 6, 7, 8 });
+        solution.lenLongestFibSubseq(new int[] { 1, 3, 7, 11, 12, 14, 18 });
+        solution.lenLongestFibSubseq(new int[] { 2, 4, 7, 8, 9, 10, 14, 15, 18, 23,
+                32, 50 });
+        solution.lenLongestFibSubseq(new int[] { 1, 3, 7, 8, 15 });
         solution.lenLongestFibSubseq(new int[] { 1, 3, 5 });
         solution.lenLongestFibSubseq(new int[] { 1, 3, 4, 5 });
 
