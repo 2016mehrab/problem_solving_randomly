40cca8f9e6bba6574c2b72c3224fd8360af9ac89
Eshan <2016mehrab@gmail.com>
2025-03-11 16:54:35 +0600

    feat: solve 'Count of Substrings Containing Every Vowel and K Consonants II'
    find substring containing at least k - at least (k+1)

--- lc3306.java
+++ lc3306.java
@@ -1,86 +1,55 @@
+import java.util.HashMap;
+import java.util.Map;
+
 class LC3306 {
-    private boolean isVowel(char ch){
+    private boolean isVowel(char ch) {
 
         return ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u';
     }
-    public long countOfSubstrings(String word, int k) {
-        long numValidSubStrings =0;
-        Map<Character, Integer> vowelCount = new HashMap<>();
+
+    private long atLeastKConsonants(String word, int k) {
+        long res = 0;
+        Map<Character, Integer> vowelMap = new HashMap<>();
         int consonantCount = 0;
-        int [] nextConsonant = new int[word.length()];
-        int nextConsonantIndex = word.length();
-        for(int i = word.length() -1; i>=0; --i){
-            nextConsonant[i] =nextConsonantIndex;
- if (!isVowel(word.charAt(i))) {
-                nextConsonantIndex = i;
-            }
-        }
- int start = 0;
-        int end = 0;
- while (end < word.length()) {
-            // insert new letter
-            char newLetter = word.charAt(end);
-
-            // update counts
-            if (isVowel(newLetter)) {
-                vowelCount.put(
-                    newLetter,
-                    vowelCount.getOrDefault(newLetter, 0) + 1
-                );
+        int l = 0;
+        for (int r = 0; r < word.length(); ++r) {
+            char tmp = word.charAt(r);
+            if (isVowel(tmp)) {
+                vowelMap.putIfAbsent(tmp, 0);
+                vowelMap.put(tmp, vowelMap.get(tmp) + 1);
             } else {
-                consonantCount++;
+                ++consonantCount;
             }
-
-            // shrink window if too many consonants in our window
-            while (consonantCount > k) {
-                char startLetter = word.charAt(start);
-                if (isVowel(startLetter)) {
-                    vowelCount.put(
-                        startLetter,
-                        vowelCount.get(startLetter) - 1
-                    );
-                    if (vowelCount.get(startLetter) == 0) {
-                        vowelCount.remove(startLetter);
-                    }
-                } else {
-                    consonantCount--;
-                }
-                start++;
+            while (vowelMap.size() == 5 && consonantCount >= k) {
+                res += word.length() - r;
+                char tmp2 = word.charAt(l);
+                if (isVowel(tmp2)) {
+                    // dec vowel
+                    vowelMap.put(tmp2, vowelMap.get(tmp2) - 1);
+                    if (vowelMap.get(tmp2) == 0)
+                        vowelMap.remove(tmp2);
+                } else
+                    --consonantCount;
+                // dec consonant
+                ++l;
             }
-
-            // while we have a valid window, try to shrink it
-            while (
-                start < word.length() &&
-                vowelCount.keySet().size() == 5 &&
-                consonantCount == k
-            ) {
-                // count the current valid substring, as well as valid substrings produced by appending more vowels
-                numValidSubstrings += nextConsonant[end] - end;
-                char startLetter = word.charAt(start);
-                if (isVowel(startLetter)) {
-                    vowelCount.put(
-                        startLetter,
-                        vowelCount.get(startLetter) - 1
-                    );
-                    if (vowelCount.get(startLetter) == 0) {
-                        vowelCount.remove(startLetter);
-                    }
-                } else {
-                    consonantCount--;
-                }
-
-                start++;
-            }
-            end++;
         }
+        return res;
 
-        return numValidSubstrings;
-        
+    }
+
+    public long countOfSubstrings(String word, int k) {
+        return this.atLeastKConsonants(word, k) - this.atLeastKConsonants(word, k + 1);
+
     }
 
     public static void main(String[] args) {
 
         LC3306 solution = new LC3306();
-        
+        solution.countOfSubstrings("aeioqq", 1);
+        solution.countOfSubstrings("aeiou", 0);
+        solution.countOfSubstrings("ieaouqqieaouqq", 1);
+        solution.countOfSubstrings("iqeaouqi", 2);
+
     }
 }
